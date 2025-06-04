dc52134993d4cacd0855e0364869d32c6102e8eb
XianZS <XianZhiSen_yang@outlook.com>
2025-06-04 13:24:57 +0800

    trie Node 前缀树 struct nodetrie

---
+++ trie/func_nodetrie_applicationnode.go
@@ -0,0 +1,14 @@
+package trie
+
+// applicationNode [注册结点] 返回*NodeTree，用于存储应用程序数据。
+func (n *nodeTrie) applicationNode(part string, data string) *nodeTrie {
+	/*
+		个人见解：对于每个结点来说，它需要知道“自己在哪里？”和“自己是什么”
+		也就是说，需要知道它当前的路径和它当前的数据。
+		注册结点，那么就需要传入：
+		part string : 当前结点的路径
+		data string : 当前结点的数据
+		返回*NodeTree，用于存储应用程序数据。
+	*/
+	return nil
+}
---
+++ trie/struct_nodetrie.go
@@ -0,0 +1,14 @@
+package trie
+
+/*
+nodeTrie [结点] 用于存储应用程序数据。
+公开性质:private
+*/
+type nodeTrie struct {
+	// part [维护当前结点] 当前结点的唯一标识
+	part string
+	// children [维护子节点] 当前结点的子节点 map || slice
+	children map[string]*nodeTrie
+	// data [结点数据] 存储当前结点的数据
+	data string
+}
